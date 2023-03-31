a07c6002e07a320e7f916f84384da4a3a6dcb2da
Yana G <ivana-1@mail.ru>
2023-03-31 15:04:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAccount = 100;
+        int replenishmentAmount = 1100;
+
+        int bonus = 0;
+        if (replenishmentAmount > 1000) {
+            bonus = replenishmentAmount / 100;
+        }
+
+        int finalAccount = initialAccount + replenishmentAmount + bonus;
+        System.out.println("Итоговый счет: " + finalAccount + "рулей");
+        System.out.println("Количество бонусных рублей:" + bonus);
+    }
+}
